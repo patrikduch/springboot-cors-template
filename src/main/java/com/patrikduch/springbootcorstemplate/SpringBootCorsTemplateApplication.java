34c1930b04b0e6e08b0f8dd8927913c1f0af8134
patrikduch <duchpatrik@icloud.com>
2024-02-20 21:26:06 +0100

    Initial commit

---
+++ src/main/java/com/patrikduch/springbootcorstemplate/SpringBootCorsTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.patrikduch.springbootcorstemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCorsTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCorsTemplateApplication.class, args);
+	}
+
+}
